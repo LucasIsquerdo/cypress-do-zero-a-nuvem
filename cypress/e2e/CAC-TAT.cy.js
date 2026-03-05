d070e315746729fe54922be02517bbcfd71fe5d2
Lucas Isquerdo <lucasisquerdo22@gmail.com>
2026-03-05 10:05:23 -0300

    Tests-Cypress Cloud-Slack

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -256,7 +256,7 @@ describe('Central de Atendimento ao Cliente TAT', () => {
       .should('have.value', 'um texto qualquer')
   })
 
-  it.only('faz uma requisição HTTP', ()=>{
+  it('faz uma requisição HTTP', ()=>{
     cy.request('GET', 'https://cac-tat-v3.s3.eu-central-1.amazonaws.com/index.html')
     .as('getRequest')
     .its('status')
