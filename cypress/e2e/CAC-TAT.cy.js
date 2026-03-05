8636763b55b20dbd2eab788573a9e98f19012e63
Lucas Isquerdo <lucasisquerdo22@gmail.com>
2026-03-05 08:35:46 -0300

    Configure the ci workflow

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -196,7 +196,7 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     })
   })
 
-  it.only('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', () =>{
+  it('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', () =>{
     cy.contains('a', 'Política de Privacidade') // não é indicado usar apenas o 'a' pois é muito genérico
       .should('have.attr', 'href', 'privacy.html')
       .and('have.attr', 'target', '_blank')
